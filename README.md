7e873a0a0f9f51551708b8b9a38bb4bb776d0e75
Jelenarog <jelena.rogic90@gmail.com>
2022-12-04 10:48:13 -0700

    updated readme and js

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # Weather-Dashboard
 
-https://www.freepik.com/free-vector/blue-cloudy-daylight-background-weather-design_3264753.htm
+## Description
+Weather Dashboard is forecast weather website where you can search and get information about forecast weather for the next 5 days.
+User can search for any city that is in Open Weather Map API.
+
+---
+## Installation 
+ 
+```
+No installation is required for this application. Users can access WEATHER-DASHBOARD by clicking on the URL provided in the ‘Usage’ section. 
+```
+---
+## Usage
+The access the WEATHER-DASHBOARD webpage , click the following link: 
+
+If user searches for the city they would like to see 5 day weather forecast for result will be as below:
+
+![Deployed-Website](./assets/images/Deployed-Website.png)
+---
+## Links
+[Github Repository Link](https://github.com/Jelenarog/Weather-Dashboard)<br/>
+ [Deployed website]()<br/>
+ [HTML code](/assets/images/HTML-code.png)<br/>
+ [JavaScript](/assets/images/Js-code.png)
+
+
+---
+
+## Credits
+ [Background Image](https://www.freepik.com/free-vector/blue-cloudy-daylight-background-weather-design_3264753.htm)<br/>
+Third-Party APIs: https://openweathermap.org/forecast5
+
+---
+
+ ---
+
+## License 
+
+Please refer to the LICENSE in the repo.
+
+---
+
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,57 @@
 var formEl1 = $("#cityForm");
 var cityInput = $("#userInput");
 var displayWeather = $("#fiveDayForecast");
-var dayList = $('#searchedHistory');
+var dayList = $("#searchedHistory");
 
 var formHandler = function (event) {
   event.preventDefault();
+  $('#currentWeather').empty();
   displayWeather.empty();
   getCityLocation();
- 
+
+
 };
 
 
-var inputValidation = function(input,cityData){
-  if (input !== cityData){
-    console.log('enter valid city na');  
-  
+
+var prevSearch = [];
+
+//check if prevSearch array includes city we are searching for, if not add to existing array  and sae to local storage
+var recentSearchCheck = function (city) {
+  if (!prevSearch.includes(city)) {
+    prevSearch.push(city);
+    localStorage.setItem("cities", JSON.stringify(prevSearch));
+  }
 };
-}
 
-var prevSearch=[];
-
-var recentSearchCheck = function(city){
-if(!prevSearch.includes(city)){
-  prevSearch.push(city);
-  localStorage.setItem('cities',JSON.stringify(prevSearch));
-}
-
-}
-
-var loadCitySearched=function(){
-  prevSearch=JSON.parse(localStorage.getItem('cities'));
-  if (!prevSearch){
-    prevSearch=[];
+//create button for previously searched history
+var loadCitySearched = function () {
+  prevSearch = JSON.parse(localStorage.getItem("cities"));
+  if (!prevSearch) {
+    prevSearch = [];
   }
 
-
   dayList.empty();
- for (let i = prevSearch.length-1; i >= 0; i--) {
+  for (let i = prevSearch.length - 1; i >= 0; i--) {
  
- //console.log(prevSearch[i]);
- var searched = $("<li>").addClass("list-group-item").text(prevSearch[i]);
- 
- dayList.append(searched);
-  
- }
-}
+    var searched = $("<button>").addClass("btn btn-info my-1").text(prevSearch[i]);
+    dayList.append(searched);
+   
+    }
+
+  };
+loadCitySearched();
 
 var getCityLocation = function () {
-
-
   var citySelection = userInput.value.trim();
 
-  //fetch each city data
+  //if there is no input return, TO DO: create warning message 
+   if (citySelection ==='') {
+    
+  return;
+  }
+ 
+   else{//fetch each city data
   var apiUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     citySelection +
@@ -59,23 +59,63 @@ var getCityLocation = function () {
   fetch(apiUrl)
     .then(function (response) {
       if (response) {
-        //console.log(response);
         return response.json();
       }
     })
     .then(function (data) {
-     // console.log(data);
-     
-     recentSearchCheck(citySelection);
-    loadCitySearched();
+
+      recentSearchCheck(citySelection);
+
+      //get each city geographical coordinates
+      loadCitySearched();
       for (let i = 0; i < data.length; i++) {
-        var latCity = data[i].lat; //get each city geographical coordinates
-       // console.log(latCity);
+        var latCity = data[i].lat; 
         var lonCity = data[i].lon;
-        //console.log(lonCity);
       }
 
       //get weather forecast for selected city
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?&lat=" +
+          latCity +
+          "&lon=" +
+          lonCity +
+          "&appid=c12efca7b1f4709f10f4fbec34efb724&units=metric"
+      )
+        .then(function (response) {
+          if (response) {
+            return response.json();
+          }
+        })
+        .then(function (data) {
+
+          //get needed data from API
+          var currentDate = data.dt;
+          var currentTemperature = data.main.temp;
+          var currentHumidity = data.main.humidity;
+          var currentWind = data.wind.speed;
+          var city = data.name;
+
+          //get today's date
+          var today = dayjs();
+          var currentDate= $('<p>').text(today.format('MM/DD/YYYY'));
+          var currentDay= $("<div>").addClass("col text-center text-black bg-info bg-opacity-50 ");
+          
+          var displayCurrentTemp = $("<p>").text( "Temperature: " + currentTemperature + " ℃" );
+          var displayCurrentHum = $("<p>").text("Humidity: " + currentHumidity + ' %');
+          var displayCurrentWind = $("<p>").text("Wind speed: " + currentWind + ' MPH');
+
+          //append to HTML and display today's weather for selected city
+          currentDay.append(city);
+          currentDay.append(currentDate);
+          currentDay.append(displayCurrentTemp);
+          currentDay.append(displayCurrentHum);
+          currentDay.append(displayCurrentWind);
+         $('#currentWeather').append(currentDay);
+
+
+        });
+
+
       fetch(
         "https://api.openweathermap.org/data/2.5/forecast?&lat=" +
           latCity +
@@ -84,62 +124,67 @@ var getCityLocation = function () {
           "&appid=c12efca7b1f4709f10f4fbec34efb724&units=metric"
       )
         .then(function (response) {
-
-
           if (response) {
             return response.json();
           }
         })
-        .then(function (data) {
-         // console.log(data);
-          //console.log(data.city.name);
-          // if (data.list.length === 0){
-          //   var message = $('<p>');
-          //   message.text('Please enter valid City name.');
-          //   displayWeather.append(message);
-          //   return;
-          // }
+        .then(function (data) { 
+          console.log(data);
           var cityName = data.city.name;
-         console.log(cityName);
-          inputValidation (citySelection,cityName);
+  
+          //select only 5 days forecast weather
+          for (let i = 0; i < data.list.length; i = i + 8) {
+            var date = data.list[i].dt_txt;
+            var temperature = data.list[i].main.temp;
+            var humidity = data.list[i].main.humidity;
+            var wind = data.list[i].wind.speed;
+            var icon = data.list[i].weather[0].icon; //TO DO:show weather icon for each day
+            var iconUrl='http://openweathermap.org/img/w/'+icon+ '.png';
+            var iconImage = $('<image>').addClass('card-top-image');
+            iconImage.attr('src', iconUrl);
+            iconImage.attr('type', 'image');
+        
 
-            for (let i = 0; i < data.list.length; i = i + 8) {
-              var date = data.list[i].dt_txt;
-              var temperature = data.list[i].main.temp;
-              var humidity = data.list[i].main.humidity;
-              var wind = data.list[i].wind.speed;
-              var divCol = $("<div>").addClass("col");
-              var htmlCard = $("<div>").addClass(
-                "card shadow-lg p-3 mb-5 bg-body rounded"
-              ); // create div card
-              var cardBody = $("<div>").addClass("card-body "); //create fiv card body
-              var cardText = $("<div>").addClass(
-                "card text-black bg-info bg-opacity-50"
-              );
-              var display1 = $("<h4>").addClass("card-title").text(cityName);
-              var forecastDate = $("<p>").text(date);
-              var forecastTemp = $("<p>").text(
-                "Temperature: " + temperature + " ℃"
-              );
-              var forecastHum = $("<p>").text("Humidity: " + humidity);
-              var forecastWind = $("<p>").text("Wind speed: " + wind);
-              cardText.append(display1);
-              cardText.append(forecastDate);
-              cardText.append(forecastTemp);
-              cardText.append(forecastHum);
-              cardText.append(forecastWind);
-              cardBody.append(cardText);
-              htmlCard.append(cardBody); //card body appended to div card
-              divCol.append(htmlCard);
-              displayWeather.innerHTML='';
-              displayWeather.append(divCol);
-            }
-         
 
+            //create HTML elements and display City,temperature, wind and humidity for the next 5 days
+            var divCol = $("<div>").addClass("col");
+            var htmlCard = $("<div>").addClass( "card shadow-lg p-3 mb-5 bg-body rounded" ); 
+            var cardBody = $("<div>").addClass("card-body "); 
+            var cardText = $("<div>").addClass( "card text-black bg-info bg-opacity-50");
+            var display1 = $("<h4>").addClass("card-title").text(cityName );
+            var forecastDate = $("<p>").text(date);
+            var forecastTemp = $("<p>").text( "Temperature: " + temperature + " ℃" );
+            var forecastHum = $("<p>").text("Humidity: " + humidity + '%');
+            var forecastWind = $("<p>").text("Wind speed: " + wind + 'MPH');
+
+            //append to HTML
+            //display weather for next 5 days
+            cardText.append(display1);
+            cardText.append(iconImage);
+            cardText.append(forecastDate);
+            cardText.append(forecastTemp);
+            cardText.append(forecastHum);
+            cardText.append(forecastWind);
+            cardBody.append(cardText);
+            htmlCard.append(cardBody); 
+            divCol.append(htmlCard);
+            displayWeather.innerHTML = "";
+            displayWeather.append(divCol);
+          }
         });
     });
+}
 };
-
 formEl1.on("submit", formHandler);
 loadCitySearched();
 //console.log(prevSearch);
+
+//TO DO: if user clicks on any of previously searched Cities show 5 day forecast Weather
+dayList.on('click', function(event){
+  test=event.target;
+  userInput=test
+  getCityLocation();
+
+ }
+  )
+
